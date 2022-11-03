ed194829a57883420e20f932c0a598657e80e0e3
menisk7 <menisk7@gmail.com>
2022-11-04 00:32:49 +0100

    Refactor: cleanup

--- app/src/main/java/com/example/ocrapp/MainActivity.java
+++ app/src/main/java/com/example/ocrapp/MainActivity.java
@@ -37,7 +37,6 @@ import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
     // Storage Permissions
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
@@ -61,161 +60,109 @@ public class MainActivity extends AppCompatActivity {
     }
     //image that you can rotate using btnleft/btnright
     ImageView img;
-
     //results of ocr will be here
     EditText recognizedtext;
     Button pickimage;
     Button btnrec;
     Button btnleft;
     Button btnright;
-    int rotation;
-
     Bitmap myBitmap;
-
-
+    int rotation;
     int rotationposition=1;
+
+    //switch for btnright btnleft
+    public int nextrotation(int number) {
+        switch (number) {
+            //0 degree
+            case 1: return 0;
+            //270 degree
+            case 2: return 270;
+            //180 degree
+            case 3: return 180;
+            //90 degree
+            case 4: return 90;
+            default: throw new IllegalArgumentException();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         verifyStoragePermissions(this);
+        //All findViewById elements
         recognizedtext =findViewById(R.id.recognizedtext);
         img=findViewById(R.id.imageView);
-
-
-
-
-
-
-        //btnrec
         btnrec=findViewById(R.id.btnrec);
-        btnrec.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-            @Override
-            public void onClick(View v) {
-
-                TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
-
-
-                //imgFile = new  File(filepath);
-
-                //myBitmap = BitmapFactory.decodeFile(imgFile.getPath());
-
-                //img.setImageBitmap(myBitmap);
-
-                InputImage image = InputImage.fromBitmap(myBitmap, rotation);
-
-                Task<Text> result =
-                        recognizer.process(image)
-                                .addOnSuccessListener(new OnSuccessListener<Text>() {
-                                    @Override
-                                    public void onSuccess(Text visionText) {
-                                        // Task completed successfully
-                                        // ...
-                                        //
-                                        recognizedtext.setText(visionText.getText());
-
-
-
-                                    }
-                                })
-                                .addOnFailureListener(
-                                        new OnFailureListener() {
-                                            @Override
-                                            public void onFailure( @NonNull Exception e) {
-                                                // Task failed with an exception
-                                                // ...
-                                                if(e.getMessage().toString()=="Waiting for the text recognition module to be downloaded. Please wait.")
-                                                {
-                                                    recognizedtext.setClickable(true);
-                                                    recognizedtext.setMovementMethod(LinkMovementMethod.getInstance());
-                                                    String link = "<a href='https://play.google.com/store/apps/details?id=com.google.android.gms&referrer=utm_source%3DAndroidPIT%26utm_medium%3DAndroidPIT%26utm_campaign%3DAndroidPIT'> Update Google Play Services </a>";
-                                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                                                        //only >=24 api version
-                                                        recognizedtext.setText(Html.fromHtml(link, Html.FROM_HTML_MODE_COMPACT));
-                                                    }else{
-                                                        recognizedtext.setText(Html.fromHtml(link));
-
-                                                    }
-
-                                                }
-
-
-
-                                            }
-                                        });
-
-
-
-
-
-            }
-        });
-
-        //btnleft
-        btnleft=findViewById(R.id.btnleft);
-        btnleft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ++rotationposition;
-                if(rotationposition>4)
-                    rotationposition=1;
-                rotation=nextrotation(rotationposition);
-
-
-
-                img.setRotation(rotation);
-            }
-        });
-
-        //btnright
         btnright=findViewById(R.id.btnright);
-        btnright.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                --rotationposition;
-                if(rotationposition<1)
-                    rotationposition=4;
-
-                rotation=nextrotation(rotationposition);
-
-
-
-                img.setRotation(rotation);
-
-            }
-        });
+        btnleft=findViewById(R.id.btnleft);
+        pickimage=findViewById(R.id.pickimage);
         //disable buttons
         btnleft.setEnabled(false);
         btnright.setEnabled(false);
         btnrec.setEnabled(false);
 
-        //pick image btn
-        pickimage=findViewById(R.id.pickimage);
-        pickimage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mGetContent.launch("image/*");
-            }
+        //btnrec listener
+        btnrec.setOnClickListener(v -> {
+
+            TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
+            InputImage image = InputImage.fromBitmap(myBitmap, rotation);
+            //Listeners for recognizer.process(image)
+            Task<Text> result = recognizer.process(image).addOnSuccessListener(visionText -> {
+                // Task completed successfully
+                // ...
+                //
+                recognizedtext.setText(visionText.getText());
+            }).addOnFailureListener(
+                    e -> {
+                        // Task failed with an exception
+                        // ...
+                        //Exception that might occur if google play isn't updated
+                        //Waiting for the text recognition module to be downloaded. Please wait.
+                            if(e.getMessage()!= null)
+                            {
+                                recognizedtext.setClickable(true);
+                                recognizedtext.setMovementMethod(LinkMovementMethod.getInstance());
+                                String link = "<a href='https://play.google.com/store/apps/details?id=com.google.android.gms&referrer=utm_source%3DAndroidPIT%26utm_medium%3DAndroidPIT%26utm_campaign%3DAndroidPIT'> Update Google Play Services </a>";
+                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                    //only >=24 api version
+                                    recognizedtext.setText(Html.fromHtml(link, Html.FROM_HTML_MODE_COMPACT));
+                                }else{
+                                    recognizedtext.setText(Html.fromHtml(link));
+                                }
+                            }
+                    });
         });
-        
 
+        //btnleft listener
+        btnleft.setOnClickListener(v -> {
+            ++rotationposition;
+            if(rotationposition>4)
+                rotationposition=1;
+            rotation=nextrotation(rotationposition);
+            img.setRotation(rotation);
+        });
 
+        //btnright listener
+        btnright.setOnClickListener(v -> {
+            --rotationposition;
+            if(rotationposition<1)
+                rotationposition=4;
+            rotation=nextrotation(rotationposition);
+            img.setRotation(rotation);
+        });
 
+        //pickimage btn listener
+        pickimage.setOnClickListener(v -> mGetContent.launch("image/*"));
     }
-
-
+    //Activity Launcher so user can pick image
     ActivityResultLauncher<String> mGetContent = registerForActivityResult(new ActivityResultContracts.GetContent(),
             new ActivityResultCallback<Uri>() {
-
                 @Override
                 public void onActivityResult(Uri uri) {
-
-                    if(uri==null)
-                    {
-                        //do nothing
-                    }else {
+                    if(uri==null);
+                    else {
                         btnleft.setEnabled(true);
                         btnright.setEnabled(true);
                         btnrec.setEnabled(true);
@@ -229,30 +176,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
-
-
-
-
-
-
-    public int nextrotation(int number) {
-        switch (number) {
-            //0 degree
-            case 1: return 0;
-            //270 degree
-            case 2: return 270;
-            //180 degree
-            case 3: return 180;
-            //90 degree
-            case 4: return 90;
-
-            default: throw new IllegalArgumentException();
-
-        }
-    }
-
-
-
 }
 
 
